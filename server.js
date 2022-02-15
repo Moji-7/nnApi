51da68ddc63c2ffff03081bc9d0cc5bec700ab13
mojtabaPourmirzaei <mirzaei_7@yahoo.com>
2022-02-15 08:56:59 +0330

    creatr Api

--- server.js
+++ server.js
@@ -7,9 +7,4 @@ http
   })
   .listen(2200);
 
-http
-  .createServer(function (req, res) {
-    res.setHeader("Content-Type", "application/json");
-    res.end("JSON.stringify(result)");
-  })
-  .listen(2000);
+// comment
